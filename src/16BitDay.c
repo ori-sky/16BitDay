0f052d56218789befe480e6c6f0da34ed4d98f0d
shockk <shokku.ra@gmail.com>
2015-07-11 19:39:12 +0300

    Initial commit

---
+++ src/16BitDay.c
@@ -0,0 +1,46 @@
+#include <pebble.h>
+
+static void init() {
+	
+}
+
+static void deinit() {
+	
+}
+
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
+}
+#include <pebble.h>
+
+static void init() {
+	
+}
+
+static void deinit() {
+	
+}
+
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
+}
+#include <pebble.h>
+
+static void init() {
+	
+}
+
+static void deinit() {
+	
+}
+
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
+}
+
